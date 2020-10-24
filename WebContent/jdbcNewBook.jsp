fd921964a0c0ec2cd07d303707b4a46c2c3bac30
bc0086 <bc0086@naver.com>
2020-10-24 10:21:38 +0900

    2020-10-24 10:20 h

---
+++ WebContent/jdbcNewBook.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<script>
+$(function(){
+	
+});
+</script>
+</head>
+<body>
+	<form action="jdbcNewBook" method = "post">
+		book name : <input type="text" name ="book_name" /> <br />
+		book location : <input type="text" name = "book_location" /> <br />
+		<input type="submit" value = "book register" />
+	</form>
+</body>
+</html>
---
+++ src/JspServletStudy/jdbcNewBook.java
@@ -0,0 +1,66 @@
+package JspServletStudy;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/jdbcNewBook")
+public class jdbcNewBook extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		String bookName = request.getParameter("book_name");
+		String bookLoc = request.getParameter("book_loc");
+		
+		// 데이터베이스를 사용하기 위한 코드 
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String id = "scott";
+		String pw = "tiger";
+		
+		Connection con = null;
+		Statement stmt = null;
+		
+		try {
+			Class.forName(driver);
+			
+			con = DriverManager.getConnection(url, id, pw);
+			stmt = con.createStatement();
+			String sql = "insert into book(book_id, book_name, book_loc)";
+					sql += " values (book_seq.nextval, '" + bookName + "', '" + bookLoc + "')";
+			int result = stmt.executeUpdate(sql);
+			
+			if(result == 1) {
+				out.print("insert success!!");
+			} else {
+				out.print("insert fail!!");
+			}
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		
+		} finally {
+			try {
+				if(stmt != null) stmt.close();
+				if(con != null) con.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
